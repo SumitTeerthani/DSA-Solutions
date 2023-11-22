1027384dfa868ab6f15a3a993cc9f0e9687d7609
SumitTeerthani <sumitteerthani2002@gmail.com>
2023-11-22 23:21:46 +0530

    to find Single No

---
+++ Array/singleNo.cpp
@@ -0,0 +1,21 @@
+class Solution
+{
+public:
+    int singleNumber(vector<int> &nums)
+    {
+
+        map<int, int> mp;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            mp[nums[i]]++;
+        }
+
+        int ans;
+        for (auto x : mp)
+        {
+            if (x.second == 1)
+                ans = x.first;
+        }
+        return ans;
+    }
+};
